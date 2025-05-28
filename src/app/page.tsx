cc072db1369d4a0519eee3d877df918afc398a4c
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2025-05-28 14:32:31 +1200

    conditionally render search input, added some tailwind styling to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ type SearchedBook = {
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
   const [searchData, setSearchData] = useState<SearchedBook[]>([]);
+  const [navButtonSelected, setNavButtonSelected] = useState("Home");
 
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
@@ -31,16 +32,29 @@ export default function Home() {
     setSearchData(updatedData);
   };
 
-  console.log(searchData);
+  const selectNavButton = (string: string) => {
+    setNavButtonSelected(string);
+  };
 
   return (
-    <div>
-      <input
-        placeholder="Search title"
-        value={searchValue}
-        onChange={handleSearch}
-      />
-      <button onClick={fetchData}>Search</button>
+    <div className="p-3">
+      {navButtonSelected === "Explore" && (
+        <div className="flex justify-between mb-3">
+          <input
+            placeholder="Search title"
+            value={searchValue}
+            onChange={handleSearch}
+            className="rounded p-1"
+          />
+          <button
+            onClick={fetchData}
+            className="border border-white p-1 rounded"
+          >
+            Search
+          </button>
+        </div>
+      )}
+
       {searchData.length > 0 && (
         <ul>
           {searchData.map((book) => (
@@ -51,6 +65,11 @@ export default function Home() {
           ))}
         </ul>
       )}
+      <div className="flex justify-between fixed bottom-0 left-0 right-0 w-full p-3 z-10 bg-gray-400 text-black font-semibold">
+        <div onClick={() => selectNavButton("Home")}>Home</div>
+        <div onClick={() => selectNavButton("Explore")}>Explore</div>
+        <div onClick={() => selectNavButton("Collection")}>Collection</div>
+      </div>
     </div>
   );
 }
