db7f3592965f31ca53293ba3c5a000786359b350
RyanHazlewood0 <ryanhazlewood0@gmail.com>
2025-09-04 13:29:31 +1200

    feat: added global context to store collection data. added function to add/remove book for collection

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,19 +2,17 @@
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
-
-type BookType = {
-  title: string;
-  cover: number;
-  author: number;
-  description: string;
-};
+import { BookType } from "../../../../types";
+import { useBookContext } from "../../globalContext";
 
 const Book = () => {
   const [thisBook, setThisBook] = useState<BookType | null>(null);
   const [thisAuthor, setThisAuthor] = useState("");
+  const [isAdded, setIsAdded] = useState(false);
 
   const { id } = useParams();
+
+  const { collection, setCollection } = useBookContext();
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -26,11 +24,12 @@ const Book = () => {
           ? data.description
           : data.description?.value || "No summary available..";
 
-      const book = {
+      const book: BookType = {
         title: data.title,
         cover: data.covers[0],
         author: data.authors[0].author.key.split("s/")[1],
         description: description,
+        id: data.key.split("s/")[1],
       };
       setThisBook(book);
     };
@@ -51,24 +50,41 @@ const Book = () => {
     }
   }, [thisBook]);
 
+  const addToCollection = () => {
+    if (thisBook && !isAdded) {
+      setCollection([...collection, thisBook]);
+      setIsAdded(true);
+    } else if (thisBook && isAdded) {
+      const newData = collection.filter((book) => book.id !== thisBook.id);
+      setCollection(newData);
+      setIsAdded(false);
+    }
+  };
+
   return (
-    <div className=" fixed inset-0 z-50 flex flex-col items-center gap-5 bg-black p-3 sm:w-[640px] sm: ml-auto sm:mr-auto overflow-auto">
+    <div className="inset-0 z-50 flex flex-col items-center gap-5 bg-black p-3 sm:w-[640px] sm: ml-auto sm:mr-auto overflow-auto">
       {thisBook && (
         <>
-          <Link href={`/explore`}>
-            <button className="text-lg border rounded">
-              <p className="pr-2 pl-2 mr-auto">←</p>
-            </button>
-          </Link>
+          <div className="flex w-full">
+            <Link href={`/explore`} className="mr-auto">
+              <button className="text-lg border rounded">
+                <p className="pr-2 pl-2 mr-auto">←</p>
+              </button>
+            </Link>
+          </div>
           <img
             src={`https://covers.openlibrary.org/b/id/${thisBook.cover}-L.jpg`}
             className=" pl-20 pr-20 pb-auto"
           />
           <div className="flex flex-col gap-3 text-center w-full">
             <h1 className="text-2xl font-bold">{thisBook?.title}</h1>
-
             <h2 className="text-lg font-semibold">{thisAuthor}</h2>
-
+            <button
+              className="border rounded p-2 cursor-pointer mx-auto"
+              onClick={() => addToCollection()}
+            >
+              {isAdded ? "Remove from collection" : "Add to collection"}
+            </button>
             <hr className="border-t border-gray-300 w-full" />
             <h3 className="text-xl font-semibold">Overview</h3>
             <p>{thisBook.description}</p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/navbar/Nav";
+import { BookProvider } from "./globalContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="p-3">{children}</div>
-        <Nav />
+        <BookProvider>
+          <div className="p-3">{children}</div>
+          <Nav />
+        </BookProvider>
       </body>
     </html>
   );
